418cd8288f08511f03e6ed82a674c0308f2d76ae
Antony <dentad@users.noreply.github.com>
2020-02-18 12:23:29 +1100

    put temp sme container in a temp file, not the local src repo

--- scripts/upload-container-sme.sh
+++ scripts/upload-container-sme.sh
@@ -2,10 +2,12 @@
 
 # Run locally.
 
+SMECONTAR_NAME=$(tempfile -s .tar)
+
 # mkdir -p sme-ocidir
 # rm -rf sme-ocidir/*
 
 # podman save --compress --format oci-dir --output sme-ocidir statisticalme:latest
-podman save --compress --format docker-archive statisticalme:latest > smecon.tar
-
-rsync -av smecon.tar sme@watcherstower:
+podman save --compress --format docker-archive statisticalme:latest > "$SMECONTAR_NAME"
+rsync -av "$SMECONTAR_NAME" sme@watcherstower:smecon.tar
+rm "$SMECONTAR_NAME"
