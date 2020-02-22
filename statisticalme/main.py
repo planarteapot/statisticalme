35d98074ce571d98e440b01591a1708aa420919b
Antony <dentad@users.noreply.github.com>
2020-02-22 22:34:56 +1100

    remove two barely used aliases !in and !out from whitestar menu

--- statisticalme/main.py
+++ statisticalme/main.py
@@ -68,8 +68,6 @@ alias_matches = [
     (re.compile(r'\s*!away\b', re.IGNORECASE), ['time', 'away']),
     (re.compile(r'\s*!back\b', re.IGNORECASE), ['time', 'back']),
     (re.compile(r'\s*!dead\b', re.IGNORECASE), ['ws', 'ship', 'dead']),
-    (re.compile(r'\s*!in\b', re.IGNORECASE), ['ws', 'ship', 'in']),
-    (re.compile(r'\s*!out\b', re.IGNORECASE), ['ws', 'ship', 'out']),
     (re.compile(r'\s*!ship\b', re.IGNORECASE), ['ws', 'ship']),
     (re.compile(r'\s*!st\b', re.IGNORECASE), ['tech', 'set']),
     (re.compile(r'\s*!tr\b', re.IGNORECASE), ['tech', 'report']),
