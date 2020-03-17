030832b234f95a842acc05ab30d94d2a4574281a
Antony <dentad@users.noreply.github.com>
2020-03-17 13:08:40 +1100

    build and install python module by wheel

--- scripts/build-container-stage3-app.sh
+++ scripts/build-container-stage3-app.sh
@@ -2,13 +2,22 @@
 
 set -o errexit
 
+# Wheel prebuild
+
+/bin/rm -rf dist/*
+~/.venv/pypy_smetest1/bin/python3 setup.py bdist_wheel
+wheelname=$(ls dist/statisticalme-*-py3-none-any.whl)
+echo "MEGAFONE wheelname $wheelname"
+
+# Containerize
+
 cont=$(buildah from sme-reqs:latest)
 
 buildah config --label maintainer="Antony <dentad@users.noreply.github.com>" $cont
 
 buildah run $cont mkdir -p /opt/statisticalme /opt/statisticalme/data /opt/statisticalme/var
 
-buildah run --volume $(readlink -f .):/src_sme $cont pip3 install /src_sme
+buildah run --volume $(readlink -f .):/src_sme $cont pip3 install /src_sme/${wheelname}
 buildah copy $cont data/values-*.txt /opt/statisticalme/data
 
 buildah config --workingdir /opt/statisticalme $cont
