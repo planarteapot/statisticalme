458de8b809f27f851659ccc0ebadf6131a1bff9e
Antony <dentad@users.noreply.github.com>
2020-05-20 16:27:29 +1000

    put venv in own base path

--- scripts/build-container-stage2-reqs.sh
+++ scripts/build-container-stage2-reqs.sh
@@ -16,9 +16,10 @@ buildah run $cont apt-get update
 buildah run $cont apt-get -y install --no-install-recommends libxml2 libxslt1.1 zlib1g
 buildah run $cont apt-get -y install --no-install-recommends pypy3-dev python-pip-whl make gcc g++ libxml2-dev libxslt1-dev zlib1g-dev
 
-buildah run $cont pypy3 -m venv /root
-buildah config --env 'PATH=/root/bin:$PATH' $cont
-buildah config --env 'VIRTUAL_ENV=/root' $cont
+buildah run $cont pypy3 -m venv /root/venv-sme
+buildah config --env 'PATH=/root/venv-sme/bin:$PATH' $cont
+buildah config --env 'VIRTUAL_ENV=/root/venv-sme' $cont
+
 buildah run $cont pip3 install --no-use-pep517 wheel
 buildah run $cont pip3 install --no-use-pep517 --requirement /opt/statisticalme/requirements.txt
 
