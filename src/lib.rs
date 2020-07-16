9f290857abd4cb512ae01a96afa0fd507c2b631d
Antony <dentad@users.noreply.github.com>
2020-07-16 14:41:18 +1000

    change from cffi to pyo3

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-#[no_mangle]
-pub unsafe extern "C" fn a_function_from_rust() -> i32 {
-    42
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+fn sum_as_string(a: i64, b: i64) -> String {
+    format!("{}", a + b)
+}
+
+#[pymodule]
+fn statisticalme(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+
+    Ok(())
 }
--- statisticalme/__init__.py
+++
@@ -1,1 +0,0 @@
-from .statisticalme import ffi, lib
