651c7d500dd3b0864ce825e6cd4cb099cb104b68
Antony <dentad@users.noreply.github.com>
2020-08-11 18:38:41 +1000

    add script run-local-test.sh, assuming venv sme_debug or similar is active

---
+++ scripts/run-local-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Assume a venv sme_debug or similar is active
+
+set -o errexit
+
+# Wheel prebuild
+
+/bin/rm -rf target/wheels/
+${HOME}/.venv/sme_deploy/bin/maturin develop --binding-crate pyo3
+# wheelname=$(ls target/wheels/statisticalme-*-cp38-cp38-manylinux2010_x86_64.whl)
+# echo "Destination wheel name $wheelname"
+
+# #
+
+# pip3 install --force --no-use-pep517 "${wheelname}"
+
+python3 -m statisticalme
