6ae57c61c3a11faae245892ff92f8540f5b09754
Antony <dentad@users.noreply.github.com>
2021-06-08 07:59:21 +1000

    fix upload script

--- scripts/upload-container-sme.sh
+++ scripts/upload-container-sme.sh
@@ -8,6 +8,6 @@ SMECONTAR_NAME=$(tempfile -s .tar)
 # rm -rf sme-ocidir/*
 
 # podman save --compress --format oci-dir --output sme-ocidir statisticalme:latest
-podman save --compress --format docker-archive statisticalme:latest > "$SMECONTAR_NAME"
+podman save --format docker-archive statisticalme:latest > "$SMECONTAR_NAME"
 rsync -av --info=progress2 "$SMECONTAR_NAME" bontstowersme:smecon.tar
 rm "$SMECONTAR_NAME"
