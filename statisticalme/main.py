5d3a6953bb88bbb7b005c73f2a0c57ced360a227
Antony <dentad@users.noreply.github.com>
2022-03-23 11:46:20 +1100

    removing incomplete redstar queue alias matches

--- statisticalme/main.py
+++ statisticalme/main.py
@@ -72,9 +72,6 @@ alias_matches = [
     (re.compile(r'\s*!ship\b', re.IGNORECASE), ['ws', 'ship']),
     (re.compile(r'\s*!st\b', re.IGNORECASE), ['tech', 'set']),
     (re.compile(r'\s*!tr\b', re.IGNORECASE), ['tech', 'report']),
-    # (re.compile(r'\s*!in\b', re.IGNORECASE), ['queue', 'in']),
-    # (re.compile(r'\s*!out\b', re.IGNORECASE), ['queue', 'out']),
-    # (re.compile(r'\s*!q\b', re.IGNORECASE), ['queue', 'refresh']),
     (re.compile(r'\s*!time\s+set\b', re.IGNORECASE), ['time']),  # keeps the 'set'
     (re.compile(r'\s*!time\b', re.IGNORECASE), ['time', 'list'])
 ]
