417b6fce227fe1408622e38d54cb84c519f949af
Antony <dentad@users.noreply.github.com>
2022-06-17 11:14:40 +1000

    add rust functions for environment load and get

--- src/sme_utils.rs
+++ src/sme_utils.rs
@@ -36,12 +36,32 @@ pub fn sme_utils_shellwords(text: &str) -> PyResult<Vec<String>> {
     Ok(sme_utils_shellwords_impl(text))
 }
 
+fn sme_utils_loadenv_impl(path_env_file: &str) {
+    dotenv::from_filename(path_env_file).expect(format!("Error: failed to load environment file {}", path_env_file).as_str());
+}
+
+#[pyfunction]
+pub fn sme_utils_loadenv(path_env_file: &str) {
+    sme_utils_loadenv_impl(path_env_file);
+}
+
+fn sme_utils_getenv_impl(env_var: &str) -> String {
+    std::env::var(env_var).expect(format!("Error: failed to find environment var {}", env_var).as_str())
+}
+
+#[pyfunction]
+pub fn sme_utils_getenv(env_var: &str) -> PyResult<String> {
+    Ok(sme_utils_getenv_impl(env_var))
+}
+
 pub fn sme_utils_pymodule(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     mod_init();
 
     m.add_wrapped(wrap_pyfunction!(sme_utils_normalize_caseless))?;
     m.add_wrapped(wrap_pyfunction!(sme_utils_is_equal_caseless))?;
     m.add_wrapped(wrap_pyfunction!(sme_utils_shellwords))?;
+    m.add_wrapped(wrap_pyfunction!(sme_utils_loadenv))?;
+    m.add_wrapped(wrap_pyfunction!(sme_utils_getenv))?;
 
     Ok(())
 }
