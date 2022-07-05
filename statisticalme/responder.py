f9b47206d68587d716fe9f3868c016a13e9cba69
Antony <dentad@users.noreply.github.com>
2022-07-05 19:08:07 +1000

    increase version to 22.2.0

--- statisticalme/responder.py
+++ statisticalme/responder.py
@@ -353,7 +353,7 @@ class MainCommand:
 
     async def dev_command_info(self, params):
         info_str = "StatisticalMe"
-        info_str += "\nversion: 22.1.0"
+        info_str += "\nversion: 22.2.0"
         info_str += "\nnotes:"
         info_str += "\n  - checking indexing during sme score"
         info_str += "\nuptime: {ut}".format(
