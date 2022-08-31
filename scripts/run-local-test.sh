8434adecccf27f7f3504e300227f92df48b54fa6
Antony <dentad@users.noreply.github.com>
2022-09-01 00:39:43 +1000

    update run-local-test

--- scripts/run-local-test.sh
+++ scripts/run-local-test.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-# Assume a venv sme_debug or similar is active
+# Assume a venv sme_local or similar is active
 
 set -o errexit
 
-# Wheel prebuild
-
 /bin/rm -rf target/wheels/
 "${HOME}"/.venv/sme_deploy/bin/maturin develop --bindings pyo3
-# wheelname=$(ls target/wheels/statisticalme-*-cp38-cp38-manylinux2010_x86_64.whl)
-# echo "Destination wheel name $wheelname"
 
-# #
+# wheelname=$(ls target/wheels/statisticalme-*-cp310-cp310-linux_x86_64.whl)
+# echo "Destination wheel name $wheelname"
 
 # pip install --force "${wheelname}"
 
